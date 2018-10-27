11574363791ae0b97e1f41f9309b6a834eec20d5
JUN <jun@192.fresh.net>
2018-10-27 16:34:46 -0400

    Multip threading exit from KeyboardInterrupt

--- ThreadingEvent.py
+++ ThreadingEvent.py
@@ -6,51 +6,38 @@ import time
 
 def timer(timerName, delay, event):
     for x in range(1,11):
-        event.wait(delay)
         if not event.is_set():
-            time.sleep(delay)
             print("%s  %s:%s:%s"%(timerName,
                               time.localtime().tm_hour,
                               time.localtime().tm_min,
                               time.localtime().tm_sec))
+            time.sleep(delay)
+            # event.wait(delay)
         else: 
-            print(timerName + " is done!")
-            print("Main thread received a signal.  Time to abort!")
+            print( timerName + " received main thread a signal.  Time to abort!")
             return   
     print(timerName + " is done!")
 
 event = threading.Event()
+
 myThread1 = threading.Thread(target = timer,
                                 name = "Timer 1",
                                 args = ("Timer 1", 5, event))
 myThread2 = threading.Thread(target = timer,
                                 name = "Timer 2",
                                 args = ("Timer 2", 2, event))
-myThread1.start()
-myThread2.start()
+#myThread1.daemon=True
+#myThread2.daemon=True
+
 try:
-    myThread1.join()
-    myThread2.join()
+    myThread1.start()
+    myThread2.start()
+    while myThread1.isAlive():
+        myThread1.join(1)
+    while myThread2.isAlive():
+        myThread2.join(1)
 except KeyboardInterrupt as e:
     print("Caught the keyboard break! " + str(e))
     event.set()
-    myThread1.join()
-    myThread2.join()
-    sys.exit()
     
 print("I'm finished!")
-while 1:
-    pass
-
-'''
-for x in range(1,11):
-    #if((time.localtime().tm_sec %5) == 0):
-    
-    print("Timer 1 %s:%s:%s"%(time.localtime().tm_hour,
-                              time.localtime().tm_min,
-                              time.localtime().tm_sec))
-    print("Timer 2 %s:%s:%s"%(time.localtime().tm_hour,
-                              time.localtime().tm_min,
-                              time.localtime().tm_sec))
-    time.sleep(2)
-'''
