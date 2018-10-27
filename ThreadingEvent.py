686bbf9e12c4722ba45facb8c39f7da94a7c6dd4
yanne829 <anney829@gmail.com>
2018-10-27 15:09:04 -0400

    Added a new file and changed it to support exceptions

--- ThreadingEvent.py
+++ ThreadingEvent.py
@@ -4,32 +4,38 @@ import sys
 import threading
 import time
 
-def timer(timerName, delay):
-    try:
-        for x in range(1,11):
+def timer(timerName, delay, event):
+    for x in range(1,11):
+        event.wait(delay)
+        if not event.is_set():
             time.sleep(delay)
             print("%s  %s:%s:%s"%(timerName,
                               time.localtime().tm_hour,
                               time.localtime().tm_min,
                               time.localtime().tm_sec))
-        print(timerName + " is done!")
-    except KeyboardInterrupt:
-        print(timerName + " caught the keyboard break! ")
-        sys.exit()
-    
-try:
-    myThread1 = threading.Thread(target = timer,
+        else: 
+            print(timerName + " is done!")
+            print("Main thread received a signal.  Time to abort!")
+            return   
+    print(timerName + " is done!")
+
+event = threading.Event()
+myThread1 = threading.Thread(target = timer,
                                 name = "Timer 1",
-                                args = ("Timer 1", 5))
-    myThread2 = threading.Thread(target = timer,
+                                args = ("Timer 1", 5, event))
+myThread2 = threading.Thread(target = timer,
                                 name = "Timer 2",
-                                args = ("Timer 2", 2))
-    myThread1.start()
-    myThread2.start()
+                                args = ("Timer 2", 2, event))
+myThread1.start()
+myThread2.start()
+try:
     myThread1.join()
     myThread2.join()
 except KeyboardInterrupt as e:
     print("Caught the keyboard break! " + str(e))
+    event.set()
+    myThread1.join()
+    myThread2.join()
     sys.exit()
     
 print("I'm finished!")
